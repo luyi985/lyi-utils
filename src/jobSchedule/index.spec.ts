bb15cb47d77520e2d3ec77e1281586b1d7b2cd4d
Yi Lu <luyi985@gmail.com>
2021-08-08 19:20:16 +1000

    feat: task schedule

--- src/jobSchedule/index.spec.ts
+++ src/jobSchedule/index.spec.ts
@@ -1,7 +1,30 @@
-import { schedule } from "./schedule";
+import { schedule, Tasks } from "./schedule";
+
+const funcMock = jest.fn();
 
 describe("schedule", () => {
-  test("", () => {
-    expect(schedule([])).toEqual([]);
+  let mockAsyncTasks: Tasks = [];
+  let mockTasks: Tasks = [];
+  beforeEach(() => {
+    jest.clearAllMocks().resetAllMocks();
+    jest.useFakeTimers();
+    mockTasks = [
+      { duration: 5000, task: () => funcMock("data1") },
+      { duration: 5000, task: () => funcMock("data2") },
+    ];
+    mockAsyncTasks = [
+      { duration: 2000, task: async () => funcMock("data1") },
+      { duration: 5000, task: async () => funcMock("data2") },
+    ];
+  });
+  test("sync task", () => {
+    funcMock.mockImplementation((data) => data);
+    schedule(mockTasks);
+    expect(funcMock).toHaveBeenCalledTimes(1);
+    expect(funcMock).toHaveBeenCalledWith("data1");
+    jest.advanceTimersByTime(5000);
+    expect(funcMock).toHaveBeenCalledTimes(2);
+    expect(funcMock).toHaveBeenCalledWith("data2");
+    jest.advanceTimersByTime(5000);
   });
 });
--- src/jobSchedule/schedule.ts
+++ src/jobSchedule/schedule.ts
@@ -1,8 +1,22 @@
-type Tasks = {
+export type Tasks = {
   duration: number;
   task: () => Promise<any> | any;
 }[];
 
-export const schedule = (tasks: Tasks) => {
-  return [];
+export const schedule = async (tasks: Tasks) => {
+  let d: any;
+  try {
+    if (!Array.isArray(tasks) || tasks.length === 0) return {};
+    const currentTask = tasks.shift();
+    if (!currentTask) return {};
+    const { duration, task } = currentTask;
+    task();
+    d = setTimeout(() => {
+      clearTimeout(d);
+      schedule(tasks);
+    }, duration);
+  } catch (e) {
+    clearTimeout(d);
+    console.error(e);
+  }
 };
